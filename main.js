e3b754de40653c398e599b5998173eea282165e4
Bluelight <63112005+Bluelighto@users.noreply.github.com>
2023-02-21 11:05:25 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var count = 0;
+
+if (count > 18) {
+  function imgonnacry	() {
+   count += 2;
+   document.getElementById("applejuice").innerHTML = count;
+  }
+} 
+else {
+  function imgonnacry	() {
+   count += 1;
+   document.getElementById("applejuice").innerHTML = count;
+  }
+  if (count > 18) {
+  function imgonnacry	() {
+   count += 2;
+   document.getElementById("applejuice").innerHTML = count;
+  }
+} 
+else {
+  function imgonnacry	() {
+   count += 1;
+   document.getElementById("applejuice").innerHTML = count;
+  }
+}
+}
